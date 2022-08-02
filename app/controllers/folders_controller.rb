1613b4c7b12b2eedb13655085ac21a09f78450c8
KlimSemikin <klimsemikin@gmail.com>
2022-08-02 13:47:19 +0300

    add folder

---
+++ app/controllers/folders_controller.rb
@@ -0,0 +1,51 @@
+class FoldersController < ApplicationController
+  before_action :set_folder!, only: %i[edit update destroy]
+
+  def index
+    @folders = Folder.order created_at: :desc
+  end
+
+  def new
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = Folder.new folder_params
+
+    if @folder.save
+      respond_to do |format|
+        format.turbo_stream
+      end
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @folder.destroy
+
+    redirect_to folders_path, status: :see_other
+  end
+
+  def edit; end
+
+  def update
+    if @folder.update folder_params
+      respond_to do |format|
+        format.turbo_stream
+      end
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:title, :description)
+  end
+
+  def set_folder!
+    @folder = Folder.find(params[:id])
+  end
+end
