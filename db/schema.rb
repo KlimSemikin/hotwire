188edab86b82ca8550a4f118b8c1cb1a46c57f4c
KlimSemikin <klimsemikin@gmail.com>
2022-08-02 19:44:09 +0300

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_074220) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_153200) do
   create_table "folders", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,4 +25,20 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_074220) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "notes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "notes", "users"
 end
